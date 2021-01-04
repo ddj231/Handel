ca7e8bde8167af9cb2a6c275c1f6427e1d917c10
Daye Jack <ddj231@nyu.edu>
2021-01-04 14:48:02 -0500

    v:0.1.8.1: use updated sound url

--- HandelExport.js
+++ HandelExport.js
@@ -2,7 +2,7 @@ let production = true
 let baseUrl = './Sounds/'
 
 if(production){
-    baseUrl = "https://unpkg.com/handel-pl@0.1.4/Sounds/";
+    baseUrl = "https://unpkg.com/handel-pl@0.1.8/Sounds/";
 }
 
 const Handel = (function(){
