9f667476aaa922de3a960b82fe98d9b71aba9ae6
Daye Jack <ddj231@nyu.edu>
2021-01-04 21:28:05 -0500

    use latest version for sounds

--- HandelExport.js
+++ HandelExport.js
@@ -2,7 +2,7 @@ let production = true
 let baseUrl = './Sounds/'
 
 if(production){
-    baseUrl = "https://unpkg.com/handel-pl@0.1.9/Sounds/";
+    baseUrl = "https://unpkg.com/handel-pl@0.1.11/Sounds/";
 }
 
 const Handel = (function(){
