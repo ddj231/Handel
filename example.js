f44930cb59528ae9b0916b187f76cf2689600cec
Daye Jack <ddj231@nyu.edu>
2021-01-06 15:31:00 -0500

    update example

--- example.js
+++ example.js
@@ -1,65 +1,37 @@
 function clicked(){
     RunHandel(`
-    start chunk introriff
-  play G4, B4, E5 for 16b 
-  play C5 for 4b 
-  play A#4 for 1b 
-  play C5 for 1b 
-  play A#4 for 1b 
-  play G4 for 5b 
-  play F#4 for 8b 
-  play A#4 for 12b 
-  play E3 for 12b 
-  play G4, B4, E5 for 12b 
-  play C5 for 4b 
-  play A#4 for 1b 
-  play C5 for 1b 
-  play A#4 for 1b 
-  play G4 for 5b 
-  play F#4 for 8b 
-  play B3 for 1b 
-  play C4 for 1b 
-  play D4 for 2b 
-  play F#3 for 1b 
-  play G3 for 1b 
-  play A3 for 2b 
-  play C3 for 20b 
-  play E3 for 12b 
-  play G4, B4, E5 for 12b 
-  play C5 for 4b 
-  play A#4 for 1b 
-  play C5 for 1b 
-  play A#4 for 2b 
-  play G4 for 5b 
-  play F#4 for 8b 
-  play A#4 for 16b 
-  play F#4 for 28b 
-  play G3, F#4 for 4b 
-  play G4 for 1b 
-  play G4 for 1b 
-  play F#4 for 1b 
-  play E4 for 1b 
-  play D4 for 1b 
-  play C3, E4 for 2b 
-  play D4 for 2b 
-  play C4 for 2b 
-  play B3 for 2b 
-  play B2, D4 for 2b 
-  play B3 for 2b 
-  play A2, C4 for 2b 
-  play A3 for 2b 
-  play G2, B3 for 2b 
-  play G3 for 2b 
-  play F#2, A3 for 2b 
-  play B2, E3, G3, B3, E4 for 6b 
-  play C#2, F#3, A3, B3, E4 for 2b rest for 2b 
-  play D2, G3, B3, B3, E4 for 2b rest for 2b 
-  play C#2, F#3, A3, B3, E4 for 2b rest for 2b 
-  play D2, G3, B3, B3, E4 for 2b rest for 2b 
-  play C#2, F#3, A3, B3, E4 for 2b 
-  play B2, E3, G3, B3, E4 for 8b 
-  endchunk 
-run introriff with bpm 510, sound piano 
+    start
+	chunk mykeys 
+    	play E3, G3, A3, C3 for 1b
+        rest for 1b
+    	play G3, B3, D3, B3 for 1b
+        rest for 1b
+    	play F3, A3, C3, E3 for 1b
+        rest for 1b
+    	play B3, D3, F3, A3 for 1b
+        rest for 1b
+    endchunk
+    
+    chunk mykick
+    	rest for 1b
+    	play E2 for 1b
+    endchunk 
+    
+    chunk mysnare
+    	play E2 for 1b
+    	rest for 1b
+    endchunk 
+    
+    chunk myhats
+    	play E2 for 1b
+    	rest for 1b
+    endchunk 
+    
+    run mykeys with bpm 100, sound piano, loop for 300
+    run mykick with bpm 100, sound kick, loop for 300
+    run myhats with sound hihat, bpm 300, loop for 300
+    run mysnare with sound snare, bpm 100, loop for 300
+    
 finish
     `)
 }
