1ff28b0bf00c5d525072585f45524d076f3203c7
Daye Jack <ddj231@nyu.edu>
2021-01-30 21:27:20 -0500

    update version

--- src/HandelExport.js
+++ src/HandelExport.js
@@ -12,7 +12,7 @@ import { theWindow } from 'tone/build/esm/core/context/AudioContext';
 
 
 export const Handel = (function () {
-    console.log("%c Handel v0.7.6", "background: crimson; color: #fff; padding: 2px;");
+    console.log("%c Handel v0.7.7", "background: crimson; color: #fff; padding: 2px;");
     class FMSynth {
         constructor() {
             this.synth = new Tone.PolySynth({
